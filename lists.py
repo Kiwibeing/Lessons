5c833333c533e4629df09e7e352322fccc4424aa
Kiwibeing <75240564+Kiwibeing@users.noreply.github.com>
2022-05-07 23:30:55 +0800

    Update lists.py

--- lists.py
+++ lists.py
@@ -8,3 +8,15 @@ for number in numbers:
         max = number
 print(max)
 
+# List Comprehensions
+list = [1, 3, 5, 7, 9]
+
+# I want to create a new list by adding 1 to each element
+new_list = [x+1 for x in list]
+
+# I want to create a new list for numbers that divide evenly with 25
+new_list = [x for x in list if 25 % x == 0]
+
+# Divisors + addition of lists
+def divisors(n):
+    return [1] + [x for x in range(2, n) if n % x == 0]
