a701c0abfefce6917a735073967485e43833a1de
Kiwibeing <75240564+Kiwibeing@users.noreply.github.com>
2022-05-07 23:14:44 +0800

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,10 @@
+names = ['John', 'Mary', 'Sarah', 'Tim', 'Mary']
+print(names[2:5])
+
+numbers = [3, 6, 2, 8, 4, 10]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
+
